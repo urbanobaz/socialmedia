a8b2715a6163e9b49bfb996077f18e22124fb6ee
Urbano <urbanobaz@yahoo.com>
2019-03-20 20:11:28 -0400

    Second commit with a lot of changes

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://urbano:urbano@cluster0-h2ypw.mongodb.net/test?retryWrites=true'
+  mongoURI: 'mongodb+srv://urbano:urbano@cluster0-h2ypw.mongodb.net/test?retryWrites=true',
+  secretOrKey: 'secret'
 };
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,58 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Post model
+const Post = require('../../models/Post');
+
+// Validation
+const ValidatePostInput = require('../../validation/post');
 
 // @route   GET api/posts/test
 // @desc    Tests posts route
 // @access  Public
 router.get('/test', (req, res) => res.json({msg: "Posts works"}));
+
+// @route   GET api/posts
+// @desc    Get posts
+// @access  Public
+router.get('/', (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({ nopostsfound: 'No posts found.' }));
+});
+
+// @route   GET api/post/:id
+// @desc    Get post by id
+// @access  Public
+router.get('/:id', (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err => res.status(404).json({ nopostfound: 'No post found with that ID' }))
+});
+
+// @route   POST api/posts
+// @desc    Create post
+// @access  Private
+router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const { errors, isValid } = ValidatePostInput(req.body);
+
+  // Check Validation
+  if(!isValid) {
+    // If any errors, send 400 with errors object
+    return res.status(400).json(errors);
+  }
+
+  const newPost = new Post({
+    text: req.body.text,
+    name: req.body.name,
+    avatar: req.body.avatar,
+    user: req.user.id
+  });
+
+  newPost.save().then(post => res.json(post));
+});
 
 module.exports = router;
